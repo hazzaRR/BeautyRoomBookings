429dc7bcb4ecff8d04a84ea62a890aecc96003e9
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2022-07-28 12:41:45 +0100

    changed the styling of adding additional treatments to an appointment

--- public/viewAppointment.js
+++ public/viewAppointment.js
@@ -1,5 +1,4 @@
 import { getClients } from './reusable.js';
-console.log("lucy bridle is fit asf");
 
 //deletes an appointment from the database and all its associated treatments
 const deleteAppBtn = document.querySelector('#deleteAppoint');
@@ -26,7 +25,6 @@ const deleteTreatment = async (event) => {
     //gets the treatment id just selected
     const item = event.target;
     const treatment = item.parentElement;
-    //const treatmentID = treatment.childNodes[1].childNodes[1].innerHTML.split(" ")[1];
     const treatmentID = treatment.id
 
     //gets the appointment id
@@ -162,7 +160,27 @@ const showAvailableTreatments = async () => {
     let appID = document.querySelector('#appID').innerHTML;
     const response = await fetch(`/appointment/getAvailableTreatments?id=${appID.split("#")[1]}`);
 
-    const treatments = await response.json();
+    let treatments = await response.json();
+
+    const sectionContainer = document.querySelector('.section-container');
+
+    sectionContainer.innerHTML = "";
+
+    const divContainer = document.createElement('div');
+    //divContainer.classList.add('page');
+    sectionContainer.appendChild(divContainer);
+
+
+    const availableTreatments =  document.createElement("h4");
+    availableTreatments.classList.add('treatmentTypes');
+    availableTreatments.innerHTML = "Available Treatments";
+
+    const treatmentUl =  document.createElement("ul");
+    treatmentUl.classList.add("treatment-ul");
+
+    divContainer.appendChild(availableTreatments);
+    divContainer.appendChild(treatmentUl);
+
 
 
     const availableTreatmentsForm = document.createElement('form');
@@ -170,52 +188,96 @@ const showAvailableTreatments = async () => {
 
     for (let i = 0; i < treatments.length; i++) {
 
-        const treatmentCheck =  document.createElement("INPUT");
-        treatmentCheck.setAttribute("type", "checkbox");
-        treatmentCheck.setAttribute('id',`treatment_${treatments[i].id}`);
-        treatmentCheck.setAttribute('name',`treatment_${treatments[i].id}`);
-        treatmentCheck.classList.add('treatment');
+        const treatmentLi =  document.createElement("li");
+        treatmentLi.classList.add("treatment-li");
+        treatmentLi.id = treatments[i].id;
+        
+        const treatmentNameDiv = document.createElement('div');
+        const treatmentNameH5 = document.createElement('H5');
+        treatmentNameH5.innerHTML = treatments[i].treatmentname;
+        treatmentNameH5.classList.add("treatmentname");
+        treatmentNameDiv.appendChild(treatmentNameH5);
 
-        const treatmentNameLabel = document.createElement("label");
-        treatmentNameLabel.setAttribute('for',`treatment_${treatments[i].id}`);
-        treatmentNameLabel.innerHTML = treatments[i].treatmentname;
-        const treatmentSpan = document.createElement("span");
-        treatmentSpan.innerHTML = ` £${treatments[i].price}`;
-
-        // grab target element reference
-        const sumbitBtn = document.querySelector('#createClient');
-
-        // insert the element before target element
-        availableTreatmentsForm.appendChild(treatmentCheck);
-        availableTreatmentsForm.appendChild(treatmentNameLabel);
-        availableTreatmentsForm.appendChild(treatmentSpan);
-        availableTreatmentsForm.appendChild(document.createElement("br"));
+        const treatmentDetailsDiv = document.createElement('div');
+        const treatmentNameSpan = document.createElement('span');
+        treatmentNameSpan.classList.add('treatment-info');
+        treatmentNameSpan.innerHTML = '£' + treatments[i].price;
+        treatmentDetailsDiv.appendChild(treatmentNameSpan);
+        
+        treatmentUl.appendChild(treatmentLi);
+        treatmentLi.appendChild(treatmentNameDiv);
+        treatmentLi.appendChild(treatmentDetailsDiv);
+        
+        
+        
+        
+        // const treatmentCheck =  document.createElement("INPUT");
+        // treatmentCheck.setAttribute("type", "checkbox");
+        // treatmentCheck.setAttribute('id',`treatment_${treatments[i].id}`);
+        // treatmentCheck.setAttribute('name',`treatment_${treatments[i].id}`);
+        // treatmentCheck.classList.add('treatment');
+        
+        // const treatmentNameLabel = document.createElement("label");
+        // treatmentNameLabel.setAttribute('for',`treatment_${treatments[i].id}`);
+        // treatmentNameLabel.innerHTML = treatments[i].treatmentname;
+        // const treatmentSpan = document.createElement("span");
+        // treatmentSpan.innerHTML = ` £${treatments[i].price}`;
+        
+        // // grab target element reference
+        // const sumbitBtn = document.querySelector('#createClient');
+        
+        // // insert the element before target element
+        // availableTreatmentsForm.appendChild(treatmentCheck);
+        // availableTreatmentsForm.appendChild(treatmentNameLabel);
+        // availableTreatmentsForm.appendChild(treatmentSpan);
+        // availableTreatmentsForm.appendChild(document.createElement("br"));
     }
 
-    const submitBtn =  document.createElement("INPUT");
-    submitBtn.setAttribute("type", "submit");
-    submitBtn.id = "addTreatmentsBtn";
+    const btnDivContainer = document.createElement('div');
+    btnDivContainer.classList.add('page');
+    sectionContainer.appendChild(btnDivContainer);
+    
+    const submitBtn = document.createElement('button');
+    submitBtn.classList.add('button-31');
+    submitBtn.id = 'addTreatmentsBtn';
+    submitBtn.innerHTML = 'Add Treatments'
+    btnDivContainer.appendChild(submitBtn);
 
-    availableTreatmentsForm.appendChild(submitBtn);
+    const cancelBtn = document.createElement('button');
+    cancelBtn.classList.add('button-31');
+    cancelBtn.id = 'cancelBtn';
+    cancelBtn.innerHTML = 'Cancel'
+    btnDivContainer.appendChild(cancelBtn);
+    
 
-    document.body.appendChild(availableTreatmentsForm);
+    treatments = document.querySelectorAll('.treatment-li');
+
+    for (let i = 0; i < treatments.length; i++) {
+        treatments[i].addEventListener('click', () => {
+        treatments[i].classList.toggle('selected-treatment');
+        });
+    };
+
+
+    // const submitBtn =  document.createElement("INPUT");
+    // submitBtn.setAttribute("type", "submit");
+    // submitBtn.id = "addTreatmentsBtn";
+
+    // availableTreatmentsForm.appendChild(submitBtn);
+
+    // document.body.appendChild(availableTreatmentsForm);
 
 
     submitBtn.addEventListener('click', async (event) => {
 
         event.preventDefault();
-
-        console.log("hey laddy");
-
         const id = appID.split("#")[1];
         let treatments = [];
 
         //gets all the selected treatments
-        let inputElements = document.getElementsByClassName('treatment');
-        for (let i=0; i < inputElements.length; i++){
-          if(inputElements[i].checked) {
-            treatments.push(inputElements[i].id.substring(10));
-          }
+        let inputElements = document.getElementsByClassName('selected-treatment');
+        for (let i=0; i < inputElements.length; i++) {
+            treatments.push(inputElements[i].id);
         };
 
                 //create a new request object
@@ -224,7 +286,7 @@ const showAvailableTreatments = async () => {
             treatments
         };
 
-        // turns newAppointment object into JSON string
+        //turns newAppointment object into JSON string
         const serializedMessage = JSON.stringify(newTreatments);
 
         // posts JSON string to the server at the end point /appointment/createAppointment
@@ -242,6 +304,13 @@ const showAvailableTreatments = async () => {
 
 
     })
+
+    cancelBtn.addEventListener('click', async (event) => {
+
+        const id = appID.split("#")[1];
+
+        window.location.href = `/appointment/viewAppointment?id=${id}`
+    });
 
 
 
